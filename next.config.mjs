8bc7574b67fbcd041034f67bd9dc7a92210aff6e
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-01-30 14:13:56 +0700

    install package & setup project

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    optimizePackageImports: [
+      "@heroui/react",
+      "@phosphor-icons/react",
+      "@ckeditor/ckeditor5-react",
+      "framer-motion",
+      "embla-carousel-react",
+    ],
+  },
 };
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,56 @@
+import { heroui } from "@heroui/react";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      screens: {
+        xs: "481px",
+        sm: "641px",
+        md: "769px",
+        lg: "1025px",
+        xl: "1281px",
+        "2xl": "1440px",
+      },
+      fontFamily: {
+        sans: ["Plus Jakarta Sans", "sans-serif"],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        black: "#1B1B1B",
+        gray: {
+          DEFAULT: "#6D6D6D",
+        },
+        orange: {
+          DEFAULT: "#F58120",
+        },
+        green: {
+          DEFAULT: "#3C6649",
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    heroui({
+      themes: {
+        light: {
+          colors: {
+            primary: {
+              DEFAULT: "#F58120", // orange
+            },
+            secondary: {
+              DEFAULT: "#3C6649", // green
+            },
+          },
+        },
+      },
+    }),
+  ],
 };
 export default config;
