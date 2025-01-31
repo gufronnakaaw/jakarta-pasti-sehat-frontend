3770c9e8029904399c3e77f2b4c717a4b3ada852
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-01-31 15:07:59 +0700

    style(home): update overview section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Navbar from "@/components/navbar/Navbar";
 import Layout from "@/components/wrapper/Layout";
 import { Button } from "@heroui/react";
-import { ArrowRight } from "@phosphor-icons/react";
+import { ArrowRight, Quotes } from "@phosphor-icons/react";
 import Image from "next/image";
 
 export default function HomePage() {
@@ -15,7 +15,7 @@ export default function HomePage() {
 
         <div className="absolute left-0 top-0 z-10 flex h-full w-full items-center justify-center bg-gradient-to-tr from-green/50 to-orange/50 px-6">
           <div className="grid max-w-[950px] justify-items-center text-center">
-            <h1 className="mb-2 text-[64px] font-black uppercase leading-[100%] text-white xl:text-[70px]">
+            <h1 className="mb-2 text-[64px] font-black uppercase leading-[100%] -tracking-[2px] text-white xl:text-[70px]">
               jakarta pasti sehat
             </h1>
             <p className="font-medium leading-[180%] text-white">
@@ -45,7 +45,44 @@ export default function HomePage() {
       </section>
 
       <Layout title="Komunitas Kesehatan Yang Berkomitmen Untuk Memperkuat Kesadaran Gaya Hidup Sehat Masyarakat">
-        <section className="h-screen bg-red-200">content 1</section>
+        <section className="grid justify-items-center py-[150px]">
+          <div className="grid max-w-[600px] gap-6 lg:max-w-none lg:grid-cols-2 lg:items-center xl:gap-12">
+            <Image
+              src="/img/about-img.png"
+              alt="about img"
+              width={900}
+              height={900}
+              className="h-auto w-full"
+            />
+
+            <div className="grid">
+              <h1 className="mb-4 text-[32px] font-black capitalize leading-[100%] -tracking-[2px] text-black sm:text-[42px] xl:text-[52px]">
+                <Quotes
+                  weight="fill"
+                  size={28}
+                  className="-mt-10 inline-flex rotate-180 text-orange"
+                />{" "}
+                Sekilas tentang kami
+              </h1>
+              <p className="font-medium leading-[180%] text-gray">
+                Kami percaya bahwa kesehatan adalah hak mendasar setiap
+                individu. Dengan memberikan dukungan yang tulus, serta
+                menghadirkan program-program kesehatan yang inovatif dan
+                inklusif, kami yakin bahwa bersama-sama kita dapat menciptakan
+                Jakarta yang lebih sehat, lebih kuat, dan lebih peduli terhadap
+                kesejahteraan seluruh warganya.
+              </p>
+
+              <Button
+                color="primary"
+                endContent={<ArrowRight weight="bold" size={18} />}
+                className="mt-10 w-max px-8 font-bold"
+              >
+                Detail Tentang Kami
+              </Button>
+            </div>
+          </div>
+        </section>
 
         <section className="h-screen bg-lime-200">content 2</section>
 
