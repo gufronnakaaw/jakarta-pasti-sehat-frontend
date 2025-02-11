1af862626dc20cbab74180e66f17b31da776e6ef
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-02-11 22:32:57 +0700

    feat(sidebar): add new menu item in dashboard

--- components/sidebar/SidebarMenuDashboard.tsx
+++ components/sidebar/SidebarMenuDashboard.tsx
@@ -1,4 +1,15 @@
-import { House, Icon, Newspaper } from "@phosphor-icons/react";
+import {
+  Bag,
+  CalendarStar,
+  Camera,
+  HandHeart,
+  Handshake,
+  House,
+  Icon,
+  Image,
+  Newspaper,
+  Users,
+} from "@phosphor-icons/react";
 
 type SidebarMainMenuDashboard = {
   label: string;
@@ -7,16 +18,55 @@ type SidebarMainMenuDashboard = {
 };
 
 export function SidebarMainMenuDashboard(): SidebarMainMenuDashboard[] {
-  return [
+  const baseUrl = "/dashboard";
+
+  const mainMenuItems: SidebarMainMenuDashboard[] = [
     {
       label: "Dashboard",
-      path: "/dashboard",
+      path: `${baseUrl}`,
       icon: House,
     },
     {
+      label: "Banner",
+      path: `${baseUrl}/banners`,
+      icon: Image,
+    },
+    {
       label: "Artikel",
-      path: "/dashboard/articles",
+      path: `${baseUrl}/articles`,
       icon: Newspaper,
     },
+    {
+      label: "Event",
+      path: `${baseUrl}/events`,
+      icon: CalendarStar,
+    },
+    {
+      label: "Dokumentasi",
+      path: `${baseUrl}/documentations`,
+      icon: Camera,
+    },
+    {
+      label: "Tim",
+      path: `${baseUrl}/teams`,
+      icon: Users,
+    },
+    {
+      label: "Mitra",
+      path: `${baseUrl}/partners`,
+      icon: Handshake,
+    },
+    {
+      label: "Karir",
+      path: `${baseUrl}/careers`,
+      icon: Bag,
+    },
+    {
+      label: "Volunteer",
+      path: `${baseUrl}/volunteers`,
+      icon: HandHeart,
+    },
   ];
+
+  return mainMenuItems;
 }
