2b85a704c2e4dbd62dd3cd8953ed7f3f524517b2
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-02-11 21:59:20 +0700

    style(dashboard): add summary about web jakartapastisehat.com

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,9 +1,37 @@
+import ButtonDashbaord from "@/components/button/ButtonDashboard";
 import TitleText from "@/components/TitleText";
 import DashboardContainer from "@/components/wrapper/DashboardContainer";
 import DashboardLayout from "@/components/wrapper/DashboardLayout";
+import { formatDayWithoutTime } from "@/utils/formatDate";
+import {
+  CalendarStar,
+  HandHeart,
+  IconContext,
+  Newspaper,
+  Users,
+} from "@phosphor-icons/react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function DashboardPage() {
+  const [time, setTime] = useState(new Date());
+  const [client, setClient] = useState<boolean>(false);
+  const formatTime = (num: number) => String(num).padStart(2, "0");
+
+  useEffect(() => {
+    setClient(true);
+
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  if (!client) {
+    return;
+  }
+
   return (
     <DashboardLayout title="Dashboard" className="scrollbar-hide">
       <DashboardContainer>
@@ -26,11 +54,98 @@ export default function DashboardPage() {
             />
 
             <span className="text-sm font-bold text-black">
-              ⏰ Rabu, 5 Februari 2025 11:39
+              ⏰ {formatDayWithoutTime(new Date())}{" "}
+              {`${formatTime(time.getHours())}:${formatTime(time.getMinutes())}`}
             </span>
           </div>
 
-          <div>rangkuman data</div>
+          <div className="grid gap-6 rounded-xl border-[2px] border-gray/20 px-16 py-8">
+            <h4 className="font-bold text-black">
+              Data Web Jakarta Pasti Sehat 💊
+            </h4>
+
+            <IconContext.Provider
+              value={{
+                weight: "fill",
+                size: 64,
+                className: "text-orange",
+              }}
+            >
+              <div className="grid grid-cols-2 divide-x-2 divide-gray/20">
+                <div className="grid divide-y-2 divide-gray/20 pr-12">
+                  <div className="grid pb-8">
+                    <div className="inline-flex items-center gap-4">
+                      <Newspaper />
+
+                      <div className="flex-1">
+                        <p className="text-sm font-medium text-gray">
+                          Total Arikel
+                        </p>
+                        <h6 className="text-[32px] font-extrabold text-black">
+                          0
+                        </h6>
+                      </div>
+
+                      <ButtonDashbaord path="/articles" />
+                    </div>
+                  </div>
+
+                  <div className="grid pt-8">
+                    <div className="inline-flex items-center gap-4">
+                      <CalendarStar />
+
+                      <div className="flex-1">
+                        <p className="text-sm font-medium text-gray">
+                          Total Event
+                        </p>
+                        <h6 className="text-[32px] font-extrabold text-black">
+                          0
+                        </h6>
+                      </div>
+
+                      <ButtonDashbaord path="/events" />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="grid divide-y-2 divide-gray/20 pl-12">
+                  <div className="grid pb-8">
+                    <div className="inline-flex items-center gap-4">
+                      <HandHeart />
+
+                      <div className="flex-1">
+                        <p className="text-sm font-medium text-gray">
+                          Total Volunteer
+                        </p>
+                        <h6 className="text-[32px] font-extrabold text-black">
+                          0
+                        </h6>
+                      </div>
+
+                      <ButtonDashbaord path="/volunteers" />
+                    </div>
+                  </div>
+
+                  <div className="grid pt-8">
+                    <div className="inline-flex items-center gap-4">
+                      <Users />
+
+                      <div className="flex-1">
+                        <p className="text-sm font-medium text-gray">
+                          Total Pelamar
+                        </p>
+                        <h6 className="text-[32px] font-extrabold text-black">
+                          0
+                        </h6>
+                      </div>
+
+                      <ButtonDashbaord path="/careers" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </IconContext.Provider>
+          </div>
         </section>
       </DashboardContainer>
     </DashboardLayout>
