fbed474b2ff23de35734f8106f036e24b86a4275
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-02-13 13:31:14 +0700

    style(component): modified LoadingScreen component

--- components/loading/LoadingScreen.tsx
+++ components/loading/LoadingScreen.tsx
@@ -1,6 +1,6 @@
 export default function LoadingScreen() {
   return (
-    <div className="absolute bottom-0 right-0 z-50 flex h-[calc(100vh-5rem)] w-[calc(100%-250px)] items-center justify-center bg-[#F8F8F8]">
+    <div className="absolute bottom-0 right-0 z-50 flex h-screen w-full items-center justify-center bg-[#F8F8F8]">
       <div className="loader" />
     </div>
   );
