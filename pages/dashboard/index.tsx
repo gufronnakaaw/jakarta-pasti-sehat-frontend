682d32daacec9a83006e4ebab51d6c77d0934920
gufronnakaaw <gufronnakaaw@gmail.com>
2025-02-19 19:39:46 +0700

    feat(dashboard): implement data fetching

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,7 +1,10 @@
 import ButtonDashbaord from "@/components/button/ButtonDashboard";
+import ErrorPage from "@/components/ErrorPage";
 import TitleText from "@/components/TitleText";
 import DashboardContainer from "@/components/wrapper/DashboardContainer";
 import DashboardLayout from "@/components/wrapper/DashboardLayout";
+import { SuccessResponse } from "@/types/global";
+import { fetcher } from "@/utils/fetcher";
 import { formatDayWithoutTime } from "@/utils/formatDate";
 import {
   CalendarStar,
@@ -11,10 +14,23 @@ import {
   Newspaper,
   Users,
 } from "@phosphor-icons/react";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-export default function DashboardPage() {
+type Dashboard = {
+  total_articles: number;
+  total_events: number;
+  total_volunteers: number;
+  total_partners: number;
+  total_volappls: number;
+  total_crrappls: number;
+};
+
+export default function DashboardPage({
+  data,
+  error,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const [time, setTime] = useState(new Date());
   const [client, setClient] = useState<boolean>(false);
   const formatTime = (num: number) => String(num).padStart(2, "0");
@@ -60,125 +76,155 @@ export default function DashboardPage() {
             </span>
           </div>
 
-          <div className="grid gap-6 rounded-xl border-[2px] border-gray/15 bg-white px-16 py-8">
-            <h4 className="font-bold text-black">
-              Data Web Jakarta Pasti Sehat 💊
-            </h4>
+          {error ? (
+            <ErrorPage error={error} />
+          ) : (
+            <div className="grid gap-6 rounded-xl border-[2px] border-gray/15 bg-white px-16 py-8">
+              <h4 className="font-bold text-black">
+                Data Web Jakarta Pasti Sehat 💊
+              </h4>
 
-            <IconContext.Provider
-              value={{
-                weight: "fill",
-                size: 64,
-                className: "text-orange",
-              }}
-            >
-              <div className="grid grid-cols-2 divide-x-2 divide-gray/20">
-                <div className="grid divide-y-2 divide-gray/20 pr-12">
-                  <div className="grid pb-8">
-                    <div className="inline-flex items-center gap-4">
-                      <Newspaper />
+              <IconContext.Provider
+                value={{
+                  weight: "fill",
+                  size: 64,
+                  className: "text-orange",
+                }}
+              >
+                <div className="grid grid-cols-2 divide-x-2 divide-gray/20">
+                  <div className="grid divide-y-2 divide-gray/20 pr-12">
+                    <div className="grid pb-8">
+                      <div className="inline-flex items-center gap-4">
+                        <Newspaper />
 
-                      <div className="flex-1">
-                        <p className="text-sm font-medium text-gray">
-                          Total Arikel
-                        </p>
-                        <h6 className="text-[32px] font-extrabold text-black">
-                          0
-                        </h6>
+                        <div className="flex-1">
+                          <p className="text-sm font-medium text-gray">
+                            Total Artikel
+                          </p>
+                          <h6 className="text-[32px] font-extrabold text-black">
+                            {data?.total_articles}
+                          </h6>
+                        </div>
+
+                        <ButtonDashbaord path="/articles" />
                       </div>
+                    </div>
 
-                      <ButtonDashbaord path="/articles" />
+                    <div className="grid py-8">
+                      <div className="inline-flex items-center gap-4">
+                        <HandHeart />
+
+                        <div className="flex-1">
+                          <p className="text-sm font-medium text-gray">
+                            Total Volunteer
+                          </p>
+                          <h6 className="text-[32px] font-extrabold text-black">
+                            {data?.total_volunteers}
+                          </h6>
+                        </div>
+
+                        <ButtonDashbaord path="/volunteers" />
+                      </div>
+                    </div>
+
+                    <div className="grid pt-8">
+                      <div className="inline-flex items-center gap-4">
+                        <Users />
+
+                        <div className="flex-1">
+                          <p className="text-sm font-medium text-gray">
+                            Total Pelamar Volunteer
+                          </p>
+                          <h6 className="text-[32px] font-extrabold text-black">
+                            {data?.total_volappls}
+                          </h6>
+                        </div>
+                      </div>
                     </div>
                   </div>
 
-                  <div className="grid py-8">
-                    <div className="inline-flex items-center gap-4">
-                      <HandHeart />
+                  <div className="grid divide-y-2 divide-gray/20 pl-12">
+                    <div className="grid pb-8">
+                      <div className="inline-flex items-center gap-4">
+                        <CalendarStar />
 
-                      <div className="flex-1">
-                        <p className="text-sm font-medium text-gray">
-                          Total Volunteer
-                        </p>
-                        <h6 className="text-[32px] font-extrabold text-black">
-                          0
-                        </h6>
+                        <div className="flex-1">
+                          <p className="text-sm font-medium text-gray">
+                            Total Event
+                          </p>
+                          <h6 className="text-[32px] font-extrabold text-black">
+                            {data?.total_events}
+                          </h6>
+                        </div>
+
+                        <ButtonDashbaord path="/events" />
                       </div>
-
-                      <ButtonDashbaord path="/volunteers" />
                     </div>
-                  </div>
 
-                  <div className="grid pt-8">
-                    <div className="inline-flex items-center gap-4">
-                      <Users />
+                    <div className="grid py-8">
+                      <div className="inline-flex items-center gap-4">
+                        <Handshake />
 
-                      <div className="flex-1">
-                        <p className="text-sm font-medium text-gray">
-                          Total Pelamar Volunteer
-                        </p>
-                        <h6 className="text-[32px] font-extrabold text-black">
-                          0
-                        </h6>
+                        <div className="flex-1">
+                          <p className="text-sm font-medium text-gray">
+                            Total Mitra
+                          </p>
+                          <h6 className="text-[32px] font-extrabold text-black">
+                            {data?.total_partners}
+                          </h6>
+                        </div>
+
+                        <ButtonDashbaord path="/partners" />
+                      </div>
+                    </div>
+
+                    <div className="grid pt-8">
+                      <div className="inline-flex items-center gap-4">
+                        <Users />
+
+                        <div className="flex-1">
+                          <p className="text-sm font-medium text-gray">
+                            Total Pelamar Karir
+                          </p>
+                          <h6 className="text-[32px] font-extrabold text-black">
+                            {data?.total_crrappls}
+                          </h6>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-
-                <div className="grid divide-y-2 divide-gray/20 pl-12">
-                  <div className="grid pb-8">
-                    <div className="inline-flex items-center gap-4">
-                      <CalendarStar />
-
-                      <div className="flex-1">
-                        <p className="text-sm font-medium text-gray">
-                          Total Event
-                        </p>
-                        <h6 className="text-[32px] font-extrabold text-black">
-                          0
-                        </h6>
-                      </div>
-
-                      <ButtonDashbaord path="/events" />
-                    </div>
-                  </div>
-
-                  <div className="grid py-8">
-                    <div className="inline-flex items-center gap-4">
-                      <Handshake />
-
-                      <div className="flex-1">
-                        <p className="text-sm font-medium text-gray">
-                          Total Mitra
-                        </p>
-                        <h6 className="text-[32px] font-extrabold text-black">
-                          0
-                        </h6>
-                      </div>
-
-                      <ButtonDashbaord path="/partners" />
-                    </div>
-                  </div>
-
-                  <div className="grid pt-8">
-                    <div className="inline-flex items-center gap-4">
-                      <Users />
-
-                      <div className="flex-1">
-                        <p className="text-sm font-medium text-gray">
-                          Total Pelamar Karir
-                        </p>
-                        <h6 className="text-[32px] font-extrabold text-black">
-                          0
-                        </h6>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </IconContext.Provider>
-          </div>
+              </IconContext.Provider>
+            </div>
+          )}
         </section>
       </DashboardContainer>
     </DashboardLayout>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<{
+  data?: Dashboard;
+  error?: any;
+}> = async () => {
+  try {
+    const response: SuccessResponse<Dashboard> = await fetcher({
+      endpoint: "/dashboard",
+      method: "GET",
+      token:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbl9pZCI6IkpQU1NBMSIsInJvbGUiOiJzdXBlcmFkbWluIiwiaWF0IjoxNzM5MzM3ODgxLCJleHAiOjE3NDcxMTM4ODF9.gKAua-5M9NCQS4YTgz0t6ZgMQ_FyeGSwSaKSWO-hhpw",
+    });
+
+    return {
+      props: {
+        data: response.data,
+      },
+    };
+  } catch (error: any) {
+    return {
+      props: {
+        error,
+      },
+    };
+  }
+};
