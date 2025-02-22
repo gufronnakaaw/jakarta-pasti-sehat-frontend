c1ecdd2ab71859c770513b543907fb3052da3dea
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-02-23 00:00:27 +0700

    style(ui): modified style error page

--- components/ErrorPage.tsx
+++ components/ErrorPage.tsx
@@ -16,32 +16,33 @@ function getErrorMessage(status_code: any) {
 export default function ErrorPage({ error }: { error: any }) {
   console.error(error);
   return (
-    <section className="grid w-full justify-center gap-8 p-8">
+    <section className="flex w-full items-center justify-center p-8">
       <div className="grid gap-6">
-        <div className="grid gap-2">
-          <h1 className="text-[42px] font-black capitalize leading-[120%] -tracking-wide text-black">
-            Ups terjadi sesuatu
-          </h1>
-        </div>
+        <h1 className="text-[42px] font-black capitalize leading-[120%] -tracking-wide text-black">
+          Oppss, telah terjadi sesuatu
+        </h1>
 
-        <div className="grid items-start gap-[2px]">
+        <div className="grid items-start gap-1">
           {[
             ["Status Kode", `${error.status_code}`],
             ["Pesan Error", `${getErrorMessage(error.status_code)}`],
           ].map(([text, value], index) => (
-            <div key={index} className="grid grid-cols-[100px_2px_1fr] gap-4">
-              <h4 className="font-medium text-gray">{text}</h4>
-              <span className="font-medium text-gray">:</span>
-              <h4 className="font-extrabold text-danger">{value}</h4>
+            <div
+              key={index}
+              className="grid grid-cols-[100px_2px_1fr] gap-4 font-medium text-black"
+            >
+              <h4>{text}</h4>
+              <span>:</span>
+              <h4 className="font-bold text-danger">{value}</h4>
             </div>
           ))}
         </div>
 
         <Button
           color="primary"
-          className="mt-4 w-max px-8 font-bold"
-          startContent={<ClockClockwise />}
+          startContent={<ClockClockwise weight="bold" size={18} />}
           onPress={() => window.location.reload()}
+          className="mt-8 w-max px-12 font-bold"
         >
           Refresh
         </Button>
