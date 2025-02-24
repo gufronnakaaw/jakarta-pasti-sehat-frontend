1e050b54da87757af2b8e9d87858ab08c4547676
gufronnakaaw <gufronnakaaw@gmail.com>
2025-02-24 14:09:37 +0700

    feat(articles): implement CRD articles

---
+++ pages/dashboard/articles/create.tsx
@@ -0,0 +1,334 @@
+import ButtonBack from "@/components/button/ButtonBack";
+import ErrorPage from "@/components/ErrorPage";
+
+import TitleText from "@/components/TitleText";
+import DashboardContainer from "@/components/wrapper/DashboardContainer";
+import DashboardLayout from "@/components/wrapper/DashboardLayout";
+import { Pillar, PillarDetails } from "@/types/pillar";
+import getCroppedImg from "@/utils/cropImage";
+import { customStyleInput } from "@/utils/customStyleInput";
+import { fetcher } from "@/utils/fetcher";
+import { onCropComplete } from "@/utils/onCropComplete";
+import { Button, Input, Select, SelectItem } from "@heroui/react";
+import { FloppyDisk } from "@phosphor-icons/react";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import Cropper from "react-easy-crop";
+import toast from "react-hot-toast";
+const CKEditor = dynamic(() => import("@/components/editor/CKEditor"), {
+  ssr: false,
+});
+
+export default function CreateArticlePage({
+  pillars,
+  error,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const router = useRouter();
+
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbl9pZCI6IkpQU1NBMSIsInJvbGUiOiJzdXBlcmFkbWluIiwiaWF0IjoxNzM5MzM3ODgxLCJleHAiOjE3NDcxMTM4ODF9.gKAua-5M9NCQS4YTgz0t6ZgMQ_FyeGSwSaKSWO-hhpw";
+  const by = "Super Admin";
+
+  const [file, setFile] = useState<string | ArrayBuffer | null>();
+  const [filename, setFilename] = useState("");
+  const [type, setType] = useState("");
+  const [input, setInput] = useState({
+    title: "",
+    description: "",
+    content: "",
+  });
+  const [pillar, setPillar] = useState("");
+  const [subpillar, setSubpillar] = useState("");
+  const subPillars = pillars?.find((item) => item.pillar_id === pillar);
+
+  const [zoomImage, setZoomImage] = useState<number>(1);
+  const [cropImage, setCropImage] = useState({ x: 0, y: 0 });
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  async function handleSaveArticle() {
+    setLoading(true);
+
+    try {
+      const formData = new FormData();
+      const croppedImage = await getCroppedImg(file, croppedAreaPixels);
+
+      const response = await fetch(croppedImage as string);
+      const blob = await response.blob();
+
+      const fileConvert = new File([blob], `${filename}`, {
+        type,
+      });
+
+      formData.append("articles", fileConvert);
+      formData.append("title", input.title);
+      formData.append("description", input.description);
+      formData.append("content", input.content);
+      formData.append("pillar_id", pillar);
+      formData.append("sub_pillar_id", subpillar);
+      formData.append("by", by);
+
+      await fetcher({
+        endpoint: "/articles",
+        method: "POST",
+        file: true,
+        token,
+        data: formData,
+      });
+
+      router.back();
+      toast.success("Berhasil membuat artikel");
+    } catch (error) {
+      console.log(error);
+      toast.error("Terjadi kesalahan saat membuat artikel");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <DashboardLayout title="Buat Artikel">
+      <DashboardContainer>
+        <section className="base-dashboard">
+          <ButtonBack className="mt-0" />
+
+          <TitleText
+            title="Buat Artikel 🤝"
+            text="Buat dan kelola artikel terbaru"
+            className="border-b-2 border-dashed border-gray/20 pb-8"
+          />
+
+          {error ? (
+            <ErrorPage error={error} />
+          ) : (
+            <div className="grid max-w-[1150px] gap-8">
+              <div className="grid grid-cols-[300px_1fr] items-start gap-8">
+                <div className="grid gap-1">
+                  <div className="aspect-video size-[300px] rounded-xl border-2 border-dashed border-gray/20 p-1">
+                    <div className="relative flex h-full items-center justify-center overflow-hidden rounded-xl bg-gray/20">
+                      <Cropper
+                        image={file as string}
+                        crop={cropImage}
+                        zoom={zoomImage}
+                        aspect={1 / 1}
+                        onCropChange={setCropImage}
+                        onCropComplete={onCropComplete({
+                          setCroppedAreaPixels,
+                        })}
+                        onZoomChange={setZoomImage}
+                      />
+                    </div>
+                  </div>
+
+                  <p className="text-center text-sm font-medium leading-[170%] text-gray">
+                    <strong className="mr-1 text-danger">*</strong>ratio gambar
+                    1:1
+                  </p>
+
+                  <Input
+                    isRequired
+                    type="file"
+                    accept="image/jpg, image/jpeg, image/png"
+                    variant="flat"
+                    labelPlacement="outside"
+                    classNames={{
+                      inputWrapper: "bg-white",
+                      input:
+                        "block w-full flex-1 text-sm text-gray file:mr-4 file:py-1 file:px-3 file:border-0 file:rounded-lg file:bg-orange file:text-sm file:font-sans file:font-semibold file:text-white hover:file:bg-orange/80",
+                    }}
+                    onChange={(e) => {
+                      if (!e.target.files?.length) {
+                        setFile(null);
+                        setFilename("");
+                        setType("");
+                        return;
+                      }
+
+                      const validTypes = [
+                        "image/png",
+                        "image/jpg",
+                        "image/jpeg",
+                      ];
+
+                      if (!validTypes.includes(e.target.files[0].type)) {
+                        toast.error("Ekstensi file harus png, jpg, atau jpeg");
+                        setFile(null);
+                        setFilename("");
+                        setType("");
+                        return;
+                      }
+
+                      setType(e.target.files[0].type);
+                      setFilename(e.target.files[0].name);
+                      const reader = new FileReader();
+                      reader.readAsDataURL(e.target.files[0]);
+
+                      reader.onload = function () {
+                        setFile(reader.result);
+                      };
+
+                      reader.onerror = function (error) {
+                        setFile(null);
+                        setFilename("");
+                        setType("");
+
+                        toast.error("Terjadi kesalahan saat meload gambar");
+
+                        console.log(error);
+                      };
+                    }}
+                  />
+                </div>
+
+                <div className="grid gap-4">
+                  <Select
+                    isRequired
+                    aria-label="select pillar"
+                    variant="flat"
+                    label="Pilih Pilar"
+                    labelPlacement="outside"
+                    placeholder="Contoh: Pilar 1"
+                    name="pillar"
+                    items={pillars}
+                    selectedKeys={[pillar]}
+                    onChange={(e) => setPillar(e.target.value)}
+                    classNames={{
+                      trigger: "bg-white",
+                      value: "font-semibold text-gray",
+                    }}
+                  >
+                    {(pillar: Pillar) => (
+                      <SelectItem key={pillar.pillar_id}>
+                        {pillar.name}
+                      </SelectItem>
+                    )}
+                  </Select>
+
+                  {pillar ? (
+                    <Select
+                      isRequired
+                      aria-label="select subpillar"
+                      variant="flat"
+                      label="Pilih Subpilar"
+                      labelPlacement="outside"
+                      placeholder="Contoh: Hepatitis"
+                      name="subpillar"
+                      items={subPillars?.subpillars}
+                      selectedKeys={[subpillar]}
+                      onChange={(e) => setSubpillar(e.target.value)}
+                      classNames={{
+                        trigger: "bg-white",
+                        value: "font-semibold text-gray",
+                      }}
+                    >
+                      {(subpillar: { name: string; sub_pillar_id: string }) => (
+                        <SelectItem key={subpillar.sub_pillar_id}>
+                          {subpillar.name}
+                        </SelectItem>
+                      )}
+                    </Select>
+                  ) : null}
+
+                  <Input
+                    isRequired
+                    type="text"
+                    variant="flat"
+                    label="Judul"
+                    labelPlacement="outside"
+                    placeholder="Contoh: Artikel Terbaru Jakarta Pasti Sehat"
+                    name="alt"
+                    value={input.title}
+                    onChange={(e) =>
+                      setInput({ ...input, title: e.target.value })
+                    }
+                    classNames={{
+                      ...customStyleInput,
+                      inputWrapper: "bg-white",
+                    }}
+                  />
+
+                  <Input
+                    isRequired
+                    type="text"
+                    variant="flat"
+                    label="Deskripsi Singkat"
+                    labelPlacement="outside"
+                    placeholder="Contoh: Penyakit malaria merupakan"
+                    name="alt"
+                    value={input.description}
+                    onChange={(e) =>
+                      setInput({ ...input, description: e.target.value })
+                    }
+                    classNames={{
+                      ...customStyleInput,
+                      inputWrapper: "bg-white",
+                    }}
+                  />
+
+                  <div className="grid gap-2">
+                    <p className="font-medium text-black">Konten</p>
+
+                    <CKEditor
+                      value={input.content}
+                      onChange={(text) => {
+                        setInput({ ...input, content: text });
+                      }}
+                      token={token}
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <Button
+                isLoading={loading}
+                isDisabled={
+                  loading ||
+                  !file ||
+                  !input.title ||
+                  !input.description ||
+                  !input.content ||
+                  !pillar ||
+                  !subpillar
+                }
+                color="primary"
+                startContent={
+                  loading ? null : <FloppyDisk weight="bold" size={18} />
+                }
+                className="w-max justify-self-end font-bold"
+                onPress={handleSaveArticle}
+              >
+                Simpan
+              </Button>
+            </div>
+          )}
+        </section>
+      </DashboardContainer>
+    </DashboardLayout>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<{
+  pillars?: PillarDetails[];
+  error?: any;
+}> = async () => {
+  try {
+    const response = await fetcher({
+      endpoint: "/pillars",
+      method: "GET",
+    });
+
+    return {
+      props: {
+        pillars: response.data as PillarDetails[],
+      },
+    };
+  } catch (error: any) {
+    return {
+      props: {
+        error,
+      },
+    };
+  }
+};
--- pages/dashboard/articles/index.tsx
+++ pages/dashboard/articles/index.tsx
@@ -1,8 +1,234 @@
+import EmptyData from "@/components/EmptyData";
+import ErrorPage from "@/components/ErrorPage";
+import LoadingScreen from "@/components/loading/LoadingScreen";
+import ModalConfirmDelete from "@/components/modal/ModalConfirmDelete";
+import SearchInput from "@/components/SearchInput";
 import TitleText from "@/components/TitleText";
 import DashboardContainer from "@/components/wrapper/DashboardContainer";
 import DashboardLayout from "@/components/wrapper/DashboardLayout";
+import { useSearch } from "@/hooks/useSearch";
+import { Article } from "@/types/article";
+import { SuccessResponse } from "@/types/global";
+import { customStyleTable } from "@/utils/customStyleTable";
+import { fetcher } from "@/utils/fetcher";
+import { formatDate } from "@/utils/formatDate";
+import { getUrl } from "@/utils/string";
+import {
+  Button,
+  Chip,
+  Pagination,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@heroui/react";
+import {
+  CheckCircle,
+  IconContext,
+  Link,
+  PencilLine,
+  Plus,
+  Trash,
+  XCircle,
+} from "@phosphor-icons/react";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { ParsedUrlQuery } from "querystring";
+import { useCallback, useEffect } from "react";
+import toast from "react-hot-toast";
+import useSWR from "swr";
 
-export default function DashboardArticlesPage() {
+type ArticleResponse = {
+  articles: Article[];
+  page: number;
+  total_articles: number;
+  total_pages: number;
+};
+
+export default function DashboardArticlesPage({
+  query,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const router = useRouter();
+  const { searchValue, setSearch } = useSearch(800);
+
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbl9pZCI6IkpQU1NBMSIsInJvbGUiOiJzdXBlcmFkbWluIiwiaWF0IjoxNzM5MzM3ODgxLCJleHAiOjE3NDcxMTM4ODF9.gKAua-5M9NCQS4YTgz0t6ZgMQ_FyeGSwSaKSWO-hhpw";
+
+  const { data, isLoading, mutate, error } = useSWR<
+    SuccessResponse<ArticleResponse>
+  >({
+    endpoint: getUrl(query, "/articles", "admin"),
+    method: "GET",
+    role: "admin",
+    token: token,
+  });
+
+  useEffect(() => {
+    if (searchValue) {
+      router.push({
+        pathname: router.pathname,
+        query: {
+          ...router.query,
+          q: searchValue,
+        },
+      });
+    } else {
+      router.push({
+        pathname: router.pathname,
+      });
+    }
+  }, [searchValue]);
+
+  const columnsArticle = [
+    { name: "Gambar", uid: "image" },
+    { name: "Pilar", uid: "pillar" },
+    { name: "Judul", uid: "title" },
+    { name: "Deskripsi", uid: "description" },
+    { name: "Dibuat Pada", uid: "created_at" },
+    { name: "Status", uid: "status" },
+    { name: "Aksi", uid: "action" },
+  ];
+
+  const renderCellArticle = useCallback(
+    (article: Article, columnKey: React.Key) => {
+      const cellValue = article[columnKey as keyof Article];
+
+      switch (columnKey) {
+        case "image":
+          return (
+            <Image
+              priority
+              src={article.image_url}
+              alt={`image ${article.title}`}
+              width={500}
+              height={500}
+              className="aspect-square w-16 cursor-pointer rounded-lg object-cover object-center"
+              onClick={() => window.open(article.image_url, "_blank")}
+            />
+          );
+        case "pillar":
+          return (
+            <div className="w-[125px] font-medium text-black">
+              {article.pillar} - {article.subpillar}
+            </div>
+          );
+        case "title":
+          return (
+            <div className="w-[180px] font-medium text-black">
+              {article.title}
+            </div>
+          );
+        case "description":
+          return (
+            <div className="w-[180px] font-medium text-black">
+              {article.description}
+            </div>
+          );
+        case "created_at":
+          return (
+            <div className="w-max font-medium text-black">
+              {formatDate(article.created_at)}
+            </div>
+          );
+        case "status":
+          return (
+            <div className="w-max font-medium text-black">
+              <Chip
+                variant="flat"
+                size="sm"
+                color={article.is_active ? "success" : "danger"}
+                startContent={
+                  article.is_active ? (
+                    <CheckCircle weight="fill" size={16} />
+                  ) : (
+                    <XCircle weight="fill" size={16} />
+                  )
+                }
+                classNames={{
+                  base: "px-2 gap-1",
+                  content: "font-bold capitalize",
+                }}
+              >
+                {article.is_active ? "Aktif" : "Tidak Aktif"}
+              </Chip>
+            </div>
+          );
+        case "action":
+          return (
+            <IconContext.Provider
+              value={{
+                weight: "bold",
+                size: 18,
+                className: "text-gray",
+              }}
+            >
+              <div className="inline-flex w-max items-center gap-1">
+                <Button
+                  isIconOnly
+                  variant="light"
+                  size="sm"
+                  onPress={() =>
+                    window.open(
+                      `${window.origin}/articles/${article.slug}`,
+                      "_blank",
+                    )
+                  }
+                >
+                  <Link />
+                </Button>
+
+                <Button isIconOnly variant="light" size="sm">
+                  <PencilLine />
+                </Button>
+
+                <ModalConfirmDelete
+                  trigger={
+                    <Button isIconOnly variant="light" size="sm" color="danger">
+                      <Trash className="text-danger" />
+                    </Button>
+                  }
+                  footer={() => (
+                    <Button
+                      color="danger"
+                      className="px-6 font-bold"
+                      onPress={() => handleDeleteArticle(article.article_id)}
+                    >
+                      Ya, Hapus
+                    </Button>
+                  )}
+                />
+              </div>
+            </IconContext.Provider>
+          );
+
+        default:
+          return cellValue;
+      }
+    },
+    [],
+  );
+
+  async function handleDeleteArticle(article_id: string) {
+    try {
+      await fetcher({
+        endpoint: `/articles/${article_id}`,
+        method: "DELETE",
+        token,
+      });
+
+      mutate();
+      toast.success("Berhasil menghapus artikel");
+    } catch (error) {
+      console.log(error);
+      toast.error("Terjadi kesalahan saat menghapus artikel");
+    }
+  }
+
+  if (isLoading) return <LoadingScreen />;
+
   return (
     <DashboardLayout title="Artikel" className="scrollbar-hide">
       <DashboardContainer>
@@ -12,11 +238,92 @@ export default function DashboardArticlesPage() {
             text="Lihat dan kelola semua artikel di sini"
           />
 
-          <div>table here</div>
+          {error ? (
+            <ErrorPage error={error} />
+          ) : (
+            <div className="grid gap-4">
+              <div className="flex items-center justify-between gap-4">
+                <SearchInput
+                  placeholder="Cari Judul..."
+                  defaultValue={query.q as string}
+                  onChange={(e) => setSearch(e.target.value)}
+                  onClear={() => setSearch("")}
+                />
 
-          <div>pagination here</div>
+                <Button
+                  color="primary"
+                  startContent={<Plus weight="bold" size={18} />}
+                  className="font-bold"
+                  onPress={() => router.push("/dashboard/articles/create")}
+                >
+                  Tambah Artikel
+                </Button>
+              </div>
+
+              <div className="overflow-x-scroll scrollbar-hide">
+                <Table
+                  isStriped
+                  aria-label="article table"
+                  color="primary"
+                  selectionMode="none"
+                  classNames={customStyleTable}
+                  className="scrollbar-hide"
+                >
+                  <TableHeader columns={columnsArticle}>
+                    {(column) => (
+                      <TableColumn key={column.uid}>{column.name}</TableColumn>
+                    )}
+                  </TableHeader>
+
+                  <TableBody
+                    items={data?.data.articles}
+                    emptyContent={<EmptyData text="Article tidak ditemukan!" />}
+                  >
+                    {(article: Article) => (
+                      <TableRow key={article.article_id}>
+                        {(columnKey) => (
+                          <TableCell>
+                            {renderCellArticle(article, columnKey)}
+                          </TableCell>
+                        )}
+                      </TableRow>
+                    )}
+                  </TableBody>
+                </Table>
+              </div>
+
+              {data?.data.articles.length ? (
+                <Pagination
+                  isCompact
+                  showControls
+                  color="primary"
+                  page={data.data.page as number}
+                  total={data.data.total_pages as number}
+                  onChange={(e) => {
+                    router.push({
+                      query: {
+                        ...router.query,
+                        page: e,
+                      },
+                    });
+                  }}
+                  className="justify-self-center"
+                />
+              ) : null}
+            </div>
+          )}
         </section>
       </DashboardContainer>
     </DashboardLayout>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<{
+  query: ParsedUrlQuery;
+}> = async ({ query }) => {
+  return {
+    props: {
+      query,
+    },
+  };
+};
